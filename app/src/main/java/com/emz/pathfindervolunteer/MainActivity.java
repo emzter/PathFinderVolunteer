8d6eada3226c7b541f619f8bb3f418f3cf411be4
emzter <im_aem-ss@hotmail.com>
2017-12-09 15:02:42 +0700

    Pubnub added

--- app/src/main/java/com/emz/pathfindervolunteer/MainActivity.java
+++ app/src/main/java/com/emz/pathfindervolunteer/MainActivity.java
@@ -1,13 +1,134 @@
 package com.emz.pathfindervolunteer;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.emz.pathfindervolunteer.Utils.Constants;
+import com.emz.pathfindervolunteer.Utils.UserHelper;
+import com.emz.pathfindervolunteer.Utils.Utils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggler;
+    private NavigationView navigationView;
+    private View progressBar;
+
+    private GoogleMap mMap;
+
+    private UserHelper usrHelper;
+    private Utils utils;
+
+    private SharedPreferences mSharedPrefs;
+    private Random random;
+    private String userName;
+    private PubNub pubNub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPrefs = getSharedPreferences(Constants.DATASTREAM_PREFS, MODE_PRIVATE);
+
+        utils = new Utils(this);
+        usrHelper = new UserHelper(this);
+
+        this.random = new Random();
+        this.userName = mSharedPrefs.getString(Constants.DATASTREAM_UUID, "anonymous_" + random.nextInt(10000));
+        this.pubNub = initPubNub(this.userName);
+
+        bindView();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @NonNull
+    private PubNub initPubNub(String userName) {
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setPublishKey(Constants.PUBNUB_PUBLISH_KEY);
+        pnConfiguration.setSubscribeKey(Constants.PUBNUB_SUBSCRIBE_KEY);
+        pnConfiguration.setSecure(true);
+        pnConfiguration.setUuid(userName);
+        return new PubNub(pnConfiguration);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        pubNub.addListener(new LocationSubscribePnCallback(new LocationSubscribeMapAdapter((Activity) this.getContext(), map), Constants.SUBSCRIBE_CHANNEL_NAME));
+        pubNub.subscribe().channels(Arrays.asList(Constants.SUBSCRIBE_CHANNEL_NAME)).execute();
+        scheduleRandomUpdates();
+
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_logout:
+                onActionLogoutClicked();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void onActionLogoutClicked() {
+        usrHelper.deleteSession();
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void bindView() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        toggler = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggler);
+        toggler.syncState();
+
+        progressBar = findViewById(R.id.main_activity_progressBar);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
---
+++ app/src/main/java/com/emz/pathfindervolunteer/Utils/Constants.java
@@ -0,0 +1,13 @@
+package com.emz.pathfindervolunteer.Utils;
+
+public class Constants {
+    public static final String PUBNUB_PUBLISH_KEY = "pub-c-1964a092-0d40-4014-a6ba-9ad656bba6f4";
+    public static final String PUBNUB_SUBSCRIBE_KEY = "sub-c-67c7fa0c-dcb3-11e7-9add-92bec6890bd6";
+
+    public static final String SUBSCRIBE_CHANNEL_NAME = "basic_location_subscribe";
+    public static final String PUBLISH_CHANNEL_NAME = "basic_location_publish";
+    public static final String FLIGHTPATHS_CHANNEL_NAME = "basic_flightpaths_subscribe";
+
+    public static final String DATASTREAM_PREFS = "com.pubnub.tutorials.map.android.pubnubmaptutorialandroid.DATASTREAM_PREFS";
+    public static final String DATASTREAM_UUID = "com.pubnub.tutorials.map.android.pubnubmaptutorialandroid.DATASTREAM_UUID";
+}
