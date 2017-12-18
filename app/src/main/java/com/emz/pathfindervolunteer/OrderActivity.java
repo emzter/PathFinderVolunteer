9cea087fd5a097170ac989b2c842d679271bc1e0
emzter <im_aem-ss@hotmail.com>
2017-12-18 13:16:21 +0700

    Category

--- app/src/main/java/com/emz/pathfindervolunteer/OrderActivity.java
+++ app/src/main/java/com/emz/pathfindervolunteer/OrderActivity.java
@@ -1,8 +1,11 @@
 package com.emz.pathfindervolunteer;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -14,5 +17,26 @@ public class OrderActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(" ");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent returnIntent = getIntent();
+        setResult(RESULT_CANCELED, returnIntent);
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
